d4c9eccc8691f805489de9531c5a8deb8b05f42f
andiirgi32 <114788851+andiirgi32@users.noreply.github.com>
2024-07-15 20:39:44 +0800

    Delete admin/js directory

--- admin/js/image.js
+++
@@ -1,6 +0,0 @@
-let imgFoto = document.getElementById("imgFoto");
-let inputFoto = document.getElementById("inputFoto");
-
-inputFoto.onchange = function () {
-    imgFoto.src = URL.createObjectURL(inputFoto.files[0]);
-}
